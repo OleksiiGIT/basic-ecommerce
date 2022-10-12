9095e5f53f9d144d8ff4ac48707ead580327230c
OleksiiMatiunin1 <matalexnin@gmail.com>
2022-10-12 22:34:01 +0100

    chore: basic setup / cleanup

---
+++ src/setupTests.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import { render } from '@testing-library/react';
+
+interface ProviderProps {
+    children?: any;
+}
+
+// keep provider for future possible contexts covers
+const TheProvider: React.FC<ProviderProps> = ({ children }) => <>{children}</>;
+
+const customRender = (ui: React.ReactElement, options = {}) =>
+    render(ui, { wrapper: TheProvider, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+export * from '@testing-library/jest-dom';
+
+// override render method
+export { customRender as render };
